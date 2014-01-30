86ee2fb09fd3e35850c20ef67fad3ba3ee438aca
mwalzer <walzer@informatik.uni-tuebingen.de>
2014-01-30 17:31:17 +0100

    Added pyramid3D function based on bar3D
    drawing a pyramid shape instead of a cuboid - remark: center is in x,y,z extends to both +/- for dx and dy

--- lib/mpl_toolkits/mplot3d/axes3d.py
+++ lib/mpl_toolkits/mplot3d/axes3d.py
@@ -2394,6 +2394,112 @@ class Axes3D(Axes):
         self.add_collection(col)
 
         self.auto_scale_xyz((minx, maxx), (miny, maxy), (minz, maxz), had_data)
+        
+    def pyramid3d(self, x, y, z, dx, dy, dz, color='b',
+              zsort='average', *args, **kwargs):
+        '''
+        Generate a 3D bar, or multiple bars.
+
+        When generating multiple bars, x, y, z have to be arrays.
+        dx, dy, dz can be arrays or scalars.
+
+        *color* can be:
+
+         - A single color value, to color all bars the same color.
+
+         - An array of colors of length N bars, to color each bar
+           independently.
+
+         - An array of colors of length 6, to color the faces of the
+           bars similarly.
+
+         - An array of colors of length 6 * N bars, to color each face
+           independently.
+
+         When coloring the faces of the boxes specifically, this is
+         the order of the coloring:
+
+          1. -Z (bottom of box)
+          2. +Z (top of box)
+          3. -Y
+          4. +Y
+          5. -X
+          6. +X
+
+        Keyword arguments are passed onto
+        :func:`~mpl_toolkits.mplot3d.art3d.Poly3DCollection`
+        '''
+        had_data = self.has_data()
+
+        if not cbook.iterable(x):
+            x = [x]
+        if not cbook.iterable(y):
+            y = [y]
+        if not cbook.iterable(z):
+            z = [z]
+
+        if not cbook.iterable(dx):
+            dx = [dx]
+        if not cbook.iterable(dy):
+            dy = [dy]
+        if not cbook.iterable(dz):
+            dz = [dz]
+
+        if len(dx) == 1:
+            dx = dx * len(x)
+        if len(dy) == 1:
+            dy = dy * len(y)
+        if len(dz) == 1:
+            dz = dz * len(z)
+
+        if len(x) != len(y) or len(x) != len(z):
+            warnings.warn('x, y, and z must be the same length.')
+
+        # FIXME: This is archaic and could be done much better.
+        minx, miny, minz = 1e20, 1e20, 1e20
+        maxx, maxy, maxz = -1e20, -1e20, -1e20
+
+        polys = []
+        for xi, yi, zi, dxi, dyi, dzi in zip(x, y, z, dx, dy, dz):
+            minx = min(xi, minx)
+            maxx = max(xi + dxi, maxx)
+            miny = min(yi, miny)
+            maxy = max(yi + dyi, maxy)
+            minz = min(zi, minz)
+            maxz = max(zi + dzi, maxz)
+
+            polys.extend([
+                ((xi-dxi, yi-dyi, zi), (xi-dxi, yi+dyi, zi), (xi+dxi, yi-dyi, zi), (xi+dxi, yi+dyi, zi) ),
+                ((xi+dxi, yi-dyi, zi), (xi+dxi, yi+dyi, zi), (xi, yi, zi+dzi) ),
+                ((xi-dxi, yi-dyi, zi), (xi-dxi, yi+dyi, zi), (xi, yi, zi+dzi) ),
+                ((xi+dxi, yi-dyi, zi), (xi-dxi, yi-dyi, zi), (xi, yi, zi+dzi) ),
+                ((xi+dxi, yi+dyi, zi), (xi-dxi, yi+dyi, zi), (xi, yi, zi+dzi) ),
+            ])
+
+        facecolors = []
+        if color is None:
+            # no color specified
+            facecolors = [None] * len(x)
+        # FIXME: not working for given color(s)
+        elif len(color) == len(x):
+            # bar colors specified, need to expand to number of faces
+            for c in color:
+                facecolors.extend([c] * 6)
+        else:
+            # a single color specified, or face colors specified explicitly
+            facecolors = list(colorConverter.to_rgba_array(color))
+            if len(facecolors) < len(x):
+                facecolors *= (5 * len(x))
+
+        normals = self._generate_normals(polys)
+        sfacecolors = self._shade_colors(facecolors, normals)
+        col = art3d.Poly3DCollection(polys,
+                                     zsort=zsort,
+                                     facecolor=sfacecolors,
+                                     *args, **kwargs)
+        self.add_collection(col)
+
+        self.auto_scale_xyz((minx, maxx), (miny, maxy), (minz, maxz), had_data)
 
     def set_title(self, label, fontdict=None, loc='center', **kwargs):
         ret = Axes.set_title(self, label, fontdict=fontdict, loc=loc, **kwargs)
